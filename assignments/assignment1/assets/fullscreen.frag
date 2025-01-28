b1dd6c281327c51147427fc7d87214a0a5523df4
AuButt <austin.butt@mymail.champlain.edu>
2025-01-27 20:55:38 -0500

    class

---
+++ assignments/assignment1/assets/fullscreen.frag
@@ -0,0 +1,10 @@
+#version 450
+out vec4 fragColor;
+in vec2 vs_texCoords;
+
+uniform sampler2D texture;
+
+void main()
+{
+	
+}
---
+++ assignments/assignment1/main.cpp
@@ -0,0 +1,275 @@
+#include <stdio.h>
+#include <math.h>
+
+#include <ew/external/glad.h>
+#include <ew/shader.h>
+#include <ew/model.h>
+#include <ew/camera.h>
+#include <ew/cameraController.h>
+#include <ew/transform.h>
+#include <ew/texture.h>
+
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+GLFWwindow* initWindow(const char* title, int width, int height);
+void drawUI();
+
+struct Material
+{
+	float Ka = 1.0;
+	float Kd = 0.5;
+	float Ks = 0.5;
+	float Shininess = 128;
+}material;
+
+struct Framebuffer
+{
+	GLuint fbo;
+	GLuint color0;
+	GLuint color1;
+	GLuint color2;
+	GLuint depth;
+};
+Framebuffer framebuffer;
+
+struct FullscreenQuad {
+	GLuint vao;
+	GLuint vbo;
+
+};
+FullscreenQuad fullscreen_quad;
+
+ew::Camera camera;
+ew::CameraController cameraController;
+ew::Transform monkeyTransform;
+
+int texSlot = 0;
+
+//Global state
+int screenWidth = 1080;
+int screenHeight = 720;
+float prevFrameTime;
+float deltaTime;
+
+void resetCamera(ew::Camera* camera, ew::CameraController* controller) {
+	camera->position = glm::vec3(0, 0, 5.0f);
+	camera->target = glm::vec3(0);
+	controller->yaw = controller->pitch = 0;
+}
+
+void func(ew::Shader& shader, ew::Model &model, GLFWwindow *window, GLuint &brickTexture, GLuint& stonesTexture, GLuint& stonesNorms)
+{
+	//1. pipeline definion
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glEnable(GL_DEPTH_TEST);
+	//2. gfx pass (what goes onto screen)
+	//RENDER
+	glClearColor(0.6f, 0.8f, 0.92f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//rotate model around Y
+	monkeyTransform.rotation = glm::rotate(monkeyTransform.rotation, deltaTime, glm::vec3(0.0, 1.0, 0.0));
+
+	cameraController.move(window, &camera, deltaTime);
+
+	//bind brick to tex unit 0
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, brickTexture);
+
+	//bind brick to tex unit 1
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, stonesTexture);
+
+	//bind brick to tex unit 1
+	glActiveTexture(GL_TEXTURE2);
+	glBindTexture(GL_TEXTURE_2D, stonesNorms);
+
+	shader.use();
+	//make "_MainTex" sampler2D sample from 2D texture bound to unit 0
+	shader.setInt("_MainTex", texSlot);
+	shader.setInt("_NormalMap", 2);
+	shader.setVec3("_EyePos", camera.position);
+	shader.setMat4("transformModel", glm::mat4(1.0f));
+	shader.setMat4("viewProjection", camera.projectionMatrix() * camera.viewMatrix());
+	//Material
+	shader.setFloat("_Material.Ka", material.Ka);
+	shader.setFloat("_Material.Kd", material.Kd);
+	shader.setFloat("_Material.Ks", material.Ks);
+	shader.setFloat("_Material.Shininess", material.Shininess);
+	//transfrom.modelMatrix combines translation rotation scale into model matrix (4x4)
+	shader.setMat4("transformModel", monkeyTransform.modelMatrix());
+	model.draw(); //Draws monkey model using current shader
+
+}
+
+int main() {
+	GLFWwindow* window = initWindow("Assignment 0", screenWidth, screenHeight);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	
+
+	//textures
+	GLuint brickTexture = ew::loadTexture("assets/brick_color.jpg");
+	GLuint stonesTexture = ew::loadTexture("assets/PavingStones/PavingStones138_1K-JPG_Color.jpg");
+	GLuint stonesNorms = ew::loadTexture("assets/PavingStones/PavingStones138_1K-JPG_NormalGL.jpg");
+
+	//cache
+	ew::Shader shader = ew::Shader("assets/lit.vert", "assets/lit.frag");
+	ew::Shader fullscreen = ew::Shader("assets/fullscreen.vert", "assets/fullscreen.frag");
+	ew::Shader invShader = ew::Shader("assets/inverse.vert", "assets/inverse.frag");
+	ew::Model monkeyModel = ew::Model("assets/suzanne.fbx");
+
+	//camera
+	camera.position = glm::vec3(0.0f, 0.0f, 5.0f);
+	camera.target = glm::vec3(0.0f, 0.0f, 0.0f); //looks at center
+	camera.aspectRatio = (float)screenWidth / screenHeight;
+	camera.fov = 60.0f; //Vertical fov in degrees
+
+	//initialize fullscreen quad
+	glBindVertexArray(fullscreen_quad.vao);
+	glBindBuffer(GL_ARRAY_BUFFER, fullscreen_quad.vao);
+
+	static float quad_vertices[] = {
+		//pos (x,y) texcoords (u,v)
+		-1.0f, 1.0f, 0.0f, 1.0f,
+		-1.0f, -1.0f, 0.0f, 0.0f,
+		1.0f, -1.0f, 1.0f, 0.0,
+
+		-1.0f, 1.0f, 0.0f, 1.0f,
+		1.0f, -1.0f, 1.0f, 0.0f,
+		1.0f, 1.0f, 1.0f, 1.0f,
+	};
+
+	//bind vao to vbo
+	glBindVertexArray(fullscreen_quad.vao);
+	glBindBuffer(GL_ARRAY_BUFFER, fullscreen_quad.vbo);
+
+	//buffer data to vbo
+	glBufferData(GL_VERTEX_ARRAY, sizeof(quad_vertices), &quad_vertices, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0); //positions
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 4, (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 4, (void*) (sizeof(float) *2));
+
+
+	//--
+
+	glGenFramebuffers(1, &framebuffer.fbo);
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer.fbo);
+
+	glGenTextures(1, &framebuffer.color0);
+
+	glBindFramebuffer(GL_TEXTURE_2D, framebuffer.color0);
+	glTexImage2D();
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		float time = (float)glfwGetTime();
+		deltaTime = time - prevFrameTime;
+		prevFrameTime = time;
+
+		func(shader, monkeyModel, window, brickTexture, stonesTexture, stonesNorms);
+
+		glDisable(GL_DEPTH_TEST);
+
+		//clear default buffer
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//render fullscreen quad
+		invShader.use();
+		invShader.setInt("texture0", 0);
+		glBindVertexArray(fullscreen_quad.vao);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, framebuffer.color0);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		drawUI();
+
+		glfwSwapBuffers(window);
+	}
+	printf("Shutting down...");
+}
+
+void drawUI() {
+	ImGui_ImplGlfw_NewFrame();
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui::NewFrame();
+
+	ImGui::Begin("Settings");
+	if (ImGui::Button("Set Brick"))
+	{
+		texSlot = 0;
+	}
+	if (ImGui::Button("Set Stone"))
+	{
+		texSlot = 1;
+	}
+	if (ImGui::Button("Reset Camera"))
+	{
+		resetCamera(&camera, &cameraController);
+	}
+	if (ImGui::CollapsingHeader("Material"))
+	{
+		ImGui::SliderFloat("AmbientK", &material.Ka, 0.0f, 1.0f);
+		ImGui::SliderFloat("DiffuseK", &material.Kd, 0.0f, 1.0f);
+		ImGui::SliderFloat("SpecularK", &material.Ks, 0.0f, 1.0f);
+		ImGui::SliderFloat("Shininess", &material.Shininess, 2.0f, 1024.0f);
+	}
+	ImGui::Image((ImTextureID)(intptr_t)framebuffer.color0, ImVec2(800, 600));
+	ImGui::Text("Add Controls Here!");
+	ImGui::End();
+
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+	screenWidth = width;
+	screenHeight = height;
+}
+
+/// <summary>
+/// Initializes GLFW, GLAD, and IMGUI
+/// </summary>
+/// <param name="title">Window title</param>
+/// <param name="width">Window width</param>
+/// <param name="height">Window height</param>
+/// <returns>Returns window handle on success or null on fail</returns>
+GLFWwindow* initWindow(const char* title, int width, int height) {
+	printf("Initializing...");
+	if (!glfwInit()) {
+		printf("GLFW failed to init!");
+		return nullptr;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
+	if (window == NULL) {
+		printf("GLFW failed to create window");
+		return nullptr;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGL(glfwGetProcAddress)) {
+		printf("GLAD Failed to load GL headers");
+		return nullptr;
+	}
+
+	//Initialize ImGUI
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init();
+
+	return window;
+}
+
